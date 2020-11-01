c6e7fd58d3f3cfe31d7c2814561f4fca870ddf23
William <willspecker@gmail.com>
2020-11-01 12:25:02 +0100

    Writing test to check books in library on initialize, expanding initialize method so that book list can be called upon by typing 'library'

--- lib/visitor.rb
+++ lib/visitor.rb
@@ -1,48 +1,31 @@
 require 'yaml'
 require 'date'
+require 'pry'
 
 class Visitors
 
-    attr_accessor :library
+    attr_accessor :library, :title, :author, :available, :return_date
 
     def initialize
         @library = YAML.load_file('./lib/library.yaml')
+        @library = get_library(attrs[:library])
+    end
+    
+    def borrow_book(library)
+        # @library = YAML.load_file('./lib/library.yaml')
+        # @library[1][:available] = false
+        # @library[0][:return_date] = Date.today>>1
+        # File.open('./lib/library.yaml', 'w') { |file| file.write library.to_yaml }
     end
 
-    def borrow_book
-        library = YAML.load_file('./lib/library.yaml')
-        library[0][:available] = false
-        library[0][return_date] = Date.today>>1
-        File.open('./lib/library.yaml', 'w') { |file| file.write library.to_yaml }
-    end
 
-    def return_book 
-        library = YAML.load_file('./lib/library.yaml')
-        library[0][:available] = true
-        File.open('./lib/library.yaml', 'w') { |file| file.write library.to_yaml }
-    end
+    # def return_book 
+    #     library = YAML.load_file('./lib/library.yaml')
+    #     library[0][:available] = true
+    #     File.open('./lib/library.yaml', 'w') { |file| file.write library.to_yaml }
+    # end
 
 end
 
 
 
-# class Book 
-#     attr_accessor :title, :author, :available, :return_date
-
-#     require 'date'
-
-#     def initialize(attrs = {})
-#         @title = title
-#         @author = author
-#         @available = true
-#         @return_date = '31/11'
-#     end
-
-#     def check_out(title)
-#         if is_available?(available, return_date)
-#             { status: false, message: 'book not available', date: return_date }
-#         else
-#             check_out_book(available, return_date)
-#         end
-#     end
-# end
--- spec/library_spec.rb
+++ spec/library_spec.rb
@@ -1,17 +1,23 @@
-require './lib/library.yaml'
-require './lib/book_class.rb'
 
-describe Book do 
-    let(:book) { instance_double('Book', title: 'En Bok', author: 'En Person', available: :true, return_date: '28/11' ) }
+require './lib/visitor.rb'
 
+describe Visitors do 
+    let(:visitors) { instance_double('Book', title: 'En Bok', author: 'En Person', available: :true, return_date: '28/11' ) }
 
-    it 'can\'t check out a book that isn\'t available' do
-        allow(book).to receive(:available).and_return(false)
-        expected_output = { status: false, message: 'book not available', date: '28/11' }
-        expect(subject.check_out_book(false, '28/11')).to eq expected_output
+    it 'displays a list of books on initialize' do
+        expected_output = YAML.load_file('./lib/library.yaml')
+        expect(subject.library).to eq expected_output
     end
 
-    it 'can check out an available book' do
-        expected_output = { status: true, message: 'book available, return by ', date: Date.today } # ändra datumen till en bättre funktion
-        expect(subject.check_out_book(true, '31/11')).to eq expected_output
-    end
+
+    # it 'can\'t check out a book that isn\'t available' do
+    #     allow(book).to receive(:available).and_return(false)
+    #     expected_output = { status: false, message: 'book not available', date: '28/11' }
+    #     expect(subject.check_out_book(false, '28/11')).to eq expected_output
+    # end
+
+    # it 'can check out an available book' do
+    #     expected_output = { status: true, message: 'book available, return by ', date: Date.today } # ändra datumen till en bättre funktion
+    #     expect(subject.check_out_book(true, '31/11')).to eq expected_output
+    # end
+end
